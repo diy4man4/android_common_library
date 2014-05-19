b342d51ba737edf9b737002647cb9937cb78a3d5
zhangfei <zhangfei.jiayou@gmail.com>
2014-05-19 18:22:39 +0800

    add api in UiUtils.java

--- src/com/common/library/utils/DeviceUtils.java
+++ src/com/common/library/utils/DeviceUtils.java
@@ -17,6 +17,40 @@ import android.util.DisplayMetrics;
 import android.util.Log;
 
 public class DeviceUtils {
+	public static boolean hasFroyo() {
+        // Can use static final constants like FROYO, declared in later versions
+        // of the OS since they are inlined at compile time. This is guaranteed behavior.
+        return Build.VERSION.SDK_INT >= VERSION_CODES.FROYO;
+    }
+
+    public static boolean hasGingerbread() {
+        return Build.VERSION.SDK_INT >= VERSION_CODES.GINGERBREAD;
+    }
+
+    public static boolean hasHoneycomb() {
+        return Build.VERSION.SDK_INT >= VERSION_CODES.HONEYCOMB;
+    }
+
+    public static boolean hasHoneycombMR1() {
+        return Build.VERSION.SDK_INT >= VERSION_CODES.HONEYCOMB_MR1;
+    }
+
+    public static boolean hasIceCreamSandwich(){
+    	return Build.VERSION.SDK_INT >= VERSION_CODES.ICE_CREAM_SANDWICH;
+    }
+    
+    public static boolean hasIceCreamSandwichMR1(){
+    	return Build.VERSION.SDK_INT >= VERSION_CODES.ICE_CREAM_SANDWICH_MR1;
+    }
+    
+    public static boolean hasJellyBean() {
+        return Build.VERSION.SDK_INT >= VERSION_CODES.JELLY_BEAN;
+    }
+
+    public static boolean hasKitKat() {
+        return Build.VERSION.SDK_INT >= VERSION_CODES.KITKAT;
+    }
+    
 	public static String getDeviceId(Context context) {
 		TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
 		return telephonyManager.getDeviceId();
@@ -100,32 +134,6 @@ public class DeviceUtils {
         }
     }
 
-    public static boolean hasFroyo() {
-        // Can use static final constants like FROYO, declared in later versions
-        // of the OS since they are inlined at compile time. This is guaranteed behavior.
-        return Build.VERSION.SDK_INT >= VERSION_CODES.FROYO;
-    }
-
-    public static boolean hasGingerbread() {
-        return Build.VERSION.SDK_INT >= VERSION_CODES.GINGERBREAD;
-    }
-
-    public static boolean hasHoneycomb() {
-        return Build.VERSION.SDK_INT >= VERSION_CODES.HONEYCOMB;
-    }
-
-    public static boolean hasHoneycombMR1() {
-        return Build.VERSION.SDK_INT >= VERSION_CODES.HONEYCOMB_MR1;
-    }
-
-    public static boolean hasJellyBean() {
-        return Build.VERSION.SDK_INT >= VERSION_CODES.JELLY_BEAN;
-    }
-
-    public static boolean hasKitKat() {
-        return Build.VERSION.SDK_INT >= VERSION_CODES.KITKAT;
-    }
-    
     public static boolean haveHighQulityForVideo() {
 		if (Build.DEVICE.toLowerCase().equals("mione_plus")) {
 			return false;
--- src/com/common/library/utils/UiUtils.java
+++ src/com/common/library/utils/UiUtils.java
@@ -2,6 +2,7 @@ package com.common.library.utils;
 
 import java.util.List;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.ActivityManager;
 import android.app.ActivityManager.RunningAppProcessInfo;
@@ -10,10 +11,12 @@ import android.app.ActivityManager.RunningTaskInfo;
 import android.content.Context;
 import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
+import android.os.Build;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.PowerManager;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Toast;
 
 public class UiUtils {
@@ -186,5 +189,40 @@ public class UiUtils {
 		}
 		sLastClickTime = time;
 		return false;
+	}
+	
+	@TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH) 
+	public static void setSystemBarCanDim(Activity activity, boolean canDim){
+		if(DeviceUtils.hasIceCreamSandwich()){
+			View decorView = activity.getWindow().getDecorView();
+			int uiOptions = View.SYSTEM_UI_FLAG_LOW_PROFILE;
+			if(canDim){
+				decorView.setSystemUiVisibility(uiOptions);
+			}else{
+				// Calling setSystemUiVisibility() with a value of 0 to clears all flags.
+				decorView.setSystemUiVisibility(0);
+			}
+		}
+	}
+	
+	/**
+	 * Hidden system status bar, you can also do by set style in Manifest for activity like below:<br>
+	 * If you hide the system bars in your activity's onCreate() method and the user presses Home, 
+	 * the system bars will reappear. When the user reopens the activity, onCreate() won't get called, 
+	 * so the system bars will remain visible. If you want system UI changes to persist as the user
+	 * navigates in and out of your activity, set UI flags in onResume() or onWindowFocusChanged().
+	 * <pre>
+	 * android:theme="@android:style/Theme.Holo.NoActionBar.Fullscreen"
+	 * </pre>
+	 * @param activity
+	 */
+	@TargetApi(Build.VERSION_CODES.JELLY_BEAN) 
+	public static void hiddenStatusBar(Activity activity){
+		 if (DeviceUtils.hasJellyBean()) {
+			activity.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN );
+	    }else{
+	    	 activity.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+	                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
+	    }
 	}
 }
